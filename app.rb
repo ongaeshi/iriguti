a96ec64a98fc0c6b84700fb50b8c5be9596f4b97
ongaeshi <ongaeshi0621@gmail.com>
2014-06-29 19:36:44 +0900

    Remove puts

--- app.rb
+++ app.rb
@@ -12,14 +12,10 @@ Pocket.configure do |config|
 end
 
 get '/reset' do
-  puts "GET /reset"
   session.clear
 end
 
 get "/" do
-  puts "GET /"
-  puts "session: #{session}"
-
   unless session[:access_token]
     return '<a href="/oauth/connect">Connect with Pocket</a>'
   end
@@ -50,26 +46,14 @@ EOF
 end
 
 get "/oauth/connect" do
-  puts "OAUTH CONNECT"
   session[:code] = Pocket.get_code(:redirect_uri => CALLBACK_URL)
   new_url = Pocket.authorize_url(:code => session[:code], :redirect_uri => CALLBACK_URL)
-  puts "new_url: #{new_url}"
-  puts "session: #{session}"
   redirect new_url
 end
 
 get "/oauth/callback" do
-  puts "OAUTH CALLBACK"
-  puts "request.url: #{request.url}"
-  puts "request.body: #{request.body.read}"
   result = Pocket.get_result(session[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = result['access_token']
-  puts result['access_token']
-  puts result['username']	
-  # Alternative method to get the access token directly
-  #session[:access_token] = Pocket.get_access_token(session[:code])
-  puts session[:access_token]
-  puts "session: #{session}"
   redirect "/"
 end
 
