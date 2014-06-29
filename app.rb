94d3ab8ac0f91819ae413adf1fe76f2fd8592bbd
ongaeshi <ongaeshi0621@gmail.com>
2014-06-29 12:08:57 +0900

    Change CALLBACK_URL

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require "pocket"
 enable :sessions
 
 # CALLBACK_URL = "http://localhost:4567/oauth/callback"
-CALLBACK_URL = "http://iriguti.herokuapp.com/oauto/callback"
+CALLBACK_URL = "http://iriguti.herokuapp.com/oauth/callback"
 
 Pocket.configure do |config|
   config.consumer_key = '29328-0046aa03e13e9ff2fccd92c8'
