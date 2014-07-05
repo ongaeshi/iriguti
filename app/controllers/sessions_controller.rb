057bf9eddefcfc56ebd57679e01d227e6f20c72a
ongaeshi <ongaeshi0621@gmail.com>
2014-07-05 17:01:45 +0900

    Generate user model
    rails g model user provider:string uid:string name:string

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def create  
-    raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    session[:token] = auth["credentials"]["token"]
+    redirect_to root_url, :notice => "Signed in!"  
   end
 
   def destroy
